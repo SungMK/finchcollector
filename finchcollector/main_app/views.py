39eb95a6a527928a52ff2216a2bb3edd3e51fb30
Sung Kim <stwkim103@gmail.com>
2023-06-01 14:44:23 -0400

    complete lab part 3

--- finchcollector/main_app/views.py
+++ finchcollector/main_app/views.py
@@ -1,3 +1,4 @@
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.shortcuts import render
 from .models import Finch
 
@@ -16,3 +17,15 @@ def finches_index(request):
 def finches_detail(request, finch_id):
     finch = Finch.objects.get(id=finch_id)
     return render(request, 'finches/detail.html', {'finch': finch})
+
+class FinchCreate(CreateView):
+    model = Finch
+    fields = ['name', 'color', 'description', 'age']
+
+class FinchUpdate(UpdateView):
+    model = Finch
+    fields = ['name', 'color', 'description', 'age']
+
+class FinchDelete(DeleteView):
+    model = Finch
+    success_url = '/finches/'
